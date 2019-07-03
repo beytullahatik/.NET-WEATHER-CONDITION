7408b28dfa73e1e319fec46e614633d8ca4bd7cd
beytullahatik <51950050+beytullahatik@users.noreply.github.com>
2019-07-03 10:30:55 +0300

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HavaDurumu()
+        {
+            string api = "&APPID=02ae951ee5391e64fd1c10d83393a097";
+            string baglanti = "https://api.openweathermap.org/data/2.5/weather?q=Istanbul&mode=xml" + api;
+            XDocument Hava = XDocument.Load(baglanti);
+            var sicaklik = Hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var icon = Hava.Descendants("weather").ElementAt(0).Attribute("icon").Value;
+            var durum = Hava.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            ViewBag.icon = "http://openweathermap.org/img/w/" + icon + ".png";
+            ViewBag.sicaklik = sicaklik + "  K";
+            ViewBag.durum = durum;
+            return View();
+
+        }
+
+    }
+}
